3e9f0773eecf6aa9112453acfc4ea64c3c5446b0
Alexander <sashakostyunin1@gmail.com>
2024-01-22 00:21:46 +0200

    Rename sample bank property names to match osu!lazer

--- src/Audio/SampleBank.ts
+++ src/Audio/SampleBank.ts
@@ -19,20 +19,75 @@ export class SampleBank {
    * The bank identifier to use for the base ("hitnormal") sample.
    * Transferred to the bank of a hit sample when appropriate.
    */
-  normalSet: SampleSet = SampleSet.None;
+  bankForNormal: Lowercase<keyof typeof SampleSet> = 'none';
+
+  /**
+   * The bank identifier to use for the base ("hitnormal") sample.
+   * Transferred to the bank of a hit sample when appropriate.
+   * Use {@link bankForNormal} instead.
+   * @deprecated
+   */
+  get normalSet(): SampleSet {
+    switch (this.bankForNormal) {
+      case 'none': return SampleSet.None;
+      case 'normal': return SampleSet.Normal;
+      case 'soft': return SampleSet.Soft;
+      case 'drum': return SampleSet.Drum;
+    }
+
+    return SampleSet.None;
+  }
+
+  set normalSet(value: SampleSet) {
+    this.bankForNormal = SampleSet[value].toLowerCase() as Lowercase<keyof typeof SampleSet>;
+  }
 
   /**
    * The bank identifier to use for additions ("hitwhistle", "hitfinish", "hitclap").
    * Transferred to the bank of a hit sample when appropriate.
    */
-  additionSet: SampleSet = SampleSet.None;
+  bankForAddition: Lowercase<keyof typeof SampleSet> = 'none';
+
+  /**
+   * The bank identifier to use for additions ("hitwhistle", "hitfinish", "hitclap").
+   * Transferred to the bank of a hit sample when appropriate.
+   * Use {@link bankForAddition} instead.
+   * @deprecated
+   */
+  get additionSet(): SampleSet {
+    switch (this.bankForAddition) {
+      case 'none': return SampleSet.None;
+      case 'normal': return SampleSet.Normal;
+      case 'soft': return SampleSet.Soft;
+      case 'drum': return SampleSet.Drum;
+    }
+  }
+
+  set additionSet(value: SampleSet) {
+    this.bankForAddition = SampleSet[value].toLowerCase() as Lowercase<keyof typeof SampleSet>;
+  }
 
   /**
    * The index of the custom sample bank. Is only used if 2 or above for "reasons".
    * This will add a suffix to lookups, allowing extended bank lookups (ie. "normal-hitnormal-2").
    * See {@link HitSample.suffix}.
    */
-  customIndex = 0;
+  customSampleBank = 0;
+
+  /**
+   * The index of the custom sample bank. Is only used if 2 or above for "reasons".
+   * This will add a suffix to lookups, allowing extended bank lookups (ie. "normal-hitnormal-2").
+   * See {@link HitSample.suffix}.
+   * Use {@link customSampleBank} instead.
+   * @deprecated
+   */
+  get customIndex(): number {
+    return this.customSampleBank;
+  }
+
+  set customIndex(value: number) {
+    this.customSampleBank = value;
+  }
 
   /**
    * Creates a copy of this sample bank.
