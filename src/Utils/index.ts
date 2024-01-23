726731dbd88cfef3a5385d4bf4bda31fffe82080
Alexander <sashakostyunin1@gmail.com>
2024-01-24 01:52:13 +0200

    Revert change with readonly memory slices
    After some testing it appears that copying strings via vanilla js slice is more optimized

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -1,7 +1,6 @@
 export * as BinarySearch from './BinarySearch';
 export * as Interpolation from './Interpolation';
 export * as MathUtils from './MathUtils';
-export * as Memory from './Memory';
 
 export * from './FastRandom';
 export * from './RoundHelper';
